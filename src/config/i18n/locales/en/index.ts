453859013012e693ce1568f851c99615b071abc8
tiavina-mika <tiavinamika@gmail.com>
2024-02-02 19:37:03 +0300

    remove unused translaltion

--- src/config/i18n/locales/en/index.ts
+++ src/config/i18n/locales/en/index.ts
@@ -1,13 +1,9 @@
 import common from './common.json';
-import contact from './contact.json';
 import route from './route.json';
 import user from './user.json';
-import vehicle from './vehicle.json';
 
 export default {
   common,
   route,
-  vehicle,
   user,
-  contact,
 };
