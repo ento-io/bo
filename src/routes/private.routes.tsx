b7eb23007bdc1e611b607dd510a6b54079b8eecd
tiavina-mika <tiavinamika@gmail.com>
2024-02-08 09:08:09 +0300

    add setting page

--- src/routes/private.routes.tsx
+++ src/routes/private.routes.tsx
@@ -6,6 +6,8 @@ import Home from "@/pages/Home";
 import Profile from "@/pages/Profile";
 import articleRoutes, { articlesLayout } from "./protected/article.routes";
 import { getAppCurrentUserSelector } from "@/redux/reducers/app.reducer";
+import { PATH_NAMES } from "@/utils/pathnames";
+import Settings from "@/pages/Settings";
 
 /**
  * add id to pathless route (sub layouts)
@@ -22,7 +24,7 @@ export const privateLayout = createRoute({
       if (!user) {
         // If the user is logged out, redirect them to the login page
         throw redirect({
-          to: "/login",
+          to: PATH_NAMES.login,
             search: {
             // Use the current location to power a redirect after login
             // (Do not use `router.state.resolvedLocation` as it can
@@ -45,7 +47,13 @@ const homeRoute = createRoute({
 const profileRoute = createRoute({
   getParentRoute: () => privateLayout,
   component: Profile,
-  path: "/profile",
+  path: PATH_NAMES.profile,
+});
+
+const settingsRoute = createRoute({
+  getParentRoute: () => privateLayout,
+  component: Settings,
+  path: PATH_NAMES.settings,
 });
 
 const privateRoutes = privateLayout.addChildren([
@@ -53,6 +61,7 @@ const privateRoutes = privateLayout.addChildren([
   // use addChildren in the root because of type errors
   articlesLayout.addChildren(articleRoutes),
   profileRoute,
+  settingsRoute
 ]);
 
 export default privateRoutes;
