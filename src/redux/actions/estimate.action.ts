98fbc17726fb6f3d732d7dddf04bee438c57e264
tiavina-mika <tiavinamika@gmail.com>
2024-03-11 21:14:17 +0300

    create estimate

--- src/redux/actions/estimate.action.ts
+++ src/redux/actions/estimate.action.ts
@@ -4,9 +4,12 @@ import { PATH_NAMES } from "@/utils/pathnames";
 import { AppDispatch } from "../store";
 import { setMessageSlice } from "../reducers/app.reducer";
 import i18n from "@/config/i18n";
+import { EstimateInput } from "@/types/estimate.type";
 
-export const createEstimate = (): any => {
+export const createEstimate = (values: EstimateInput): any => {
   return actionWithLoader(async (dispatch: AppDispatch): Promise<void> => {
+  console.log('values: ', values);
+
     dispatch(setMessageSlice(i18n.t('common:estimateCreatedSuccessfully')))
   });
 };
