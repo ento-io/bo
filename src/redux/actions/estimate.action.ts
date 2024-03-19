557d097b9355652315dbf0853955f08f66dec00b
JeanM65 <jm.randrianant65@gmail.com>
2024-03-19 15:48:44 +0800

    save to DB and update store

--- src/redux/actions/estimate.action.ts
+++ src/redux/actions/estimate.action.ts
@@ -1,10 +1,11 @@
-import Parse from "parse";
+import Parse, { Attributes } from "parse";
 import { PATH_NAMES } from "@/utils/pathnames";
 import { setValues } from "@/utils/parse.utils";
 import { actionWithLoader } from "@/utils/app.utils";
 import { AppDispatch } from "../store";
 import { setMessageSlice } from "../reducers/app.reducer";
 import i18n from "@/config/i18n";
+import { addEstimateToEstimateSlice } from "../reducers/estimate.reducer";
 
 const Estimate = Parse.Object.extend("Estimate");
 
@@ -14,13 +15,15 @@ export const createEstimate = (values: any): any => {
   return actionWithLoader(async (dispatch: AppDispatch): Promise<void | undefined> => {
     const estimate = new Estimate()
 
-    setValues(estimate, values, ESTIMATE_PROPERTIES);
+    // setValues(estimate, values, ESTIMATE_PROPERTIES);
 
     estimate.set("url", values.url)
 
     const savedEstimate = await estimate.save();
+    console.log('saved Estimate:-----------:', savedEstimate);
     dispatch(setMessageSlice(i18n.t('common:estimateCreatedSuccessfully')));
-    return savedEstimate;
+    dispatch(addEstimateToEstimateSlice((savedEstimate as Attributes).toJSON()));
+    // return savedEstimate;
   });
 };
 
