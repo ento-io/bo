b06f48122396696411df2502b56bd4c84b44c7d5
tiavina-mika <tiavinamika@gmail.com>
2024-04-04 11:58:58 +0300

    Update estimate.utils.ts

--- src/utils/estimate.utils.ts
+++ src/utils/estimate.utils.ts
@@ -15,3 +15,12 @@ export const getEstimateStatusLabel = (status: string): string => {
   const statusObj = estimateStatus.find((s) => s.value === status);
   return statusObj?.value || status;
 }
+
+export const estimatesTabOptions = [
+  {
+    label: i18n.t('common:news'),
+    tab: i18n.t('common:route.new'),
+    key: 'seen',
+    value: false,
+  },
+]
