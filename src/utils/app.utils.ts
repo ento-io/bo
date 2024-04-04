dae648670d9574d9ee55b4482d3530ef46e2633d
tiavina-mika <tiavinamika@gmail.com>
2024-04-04 15:24:00 +0300

    Update app.utils.ts

--- src/utils/app.utils.ts
+++ src/utils/app.utils.ts
@@ -104,3 +104,12 @@ export const convertTabToFilters = (tabs: IListTabValue[], tabValue: IListTabVal
 
   return newFilters;
 }
+
+export const defaultTabOptions = [
+  {
+    label: i18n.t('common:recycleBin'),
+    tab: i18n.t('common:route.recycleBin'),
+    key: 'deleted',
+    value: true,
+  },
+];
