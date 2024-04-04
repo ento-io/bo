31a7fe9e5b1628087ea2142cebe1e1eb04f1ef15
tiavina-mika <tiavinamika@gmail.com>
2024-04-04 18:10:26 +0300

    Update EstimateAdvancedFilterForm.tsx

--- src/containers/estimates/EstimateAdvancedFilterForm.tsx
+++ src/containers/estimates/EstimateAdvancedFilterForm.tsx
@@ -5,19 +5,20 @@ import { useTranslation } from 'react-i18next';
 
 import AdvancedSearchButton from '@/components/buttons/AdvancedSearchButton';
 import Dialog from '@/components/Dialog';
-import TextField from '@/components/form/fields/TextField';
 import Form from '@/components/form/Form';
 
 import { useToggle } from '@/hooks/useToggle';
 
 import { EstimateFiltersInput } from '@/types/estimate.types';
 import { estimateFilterSchema } from '@/validations/estimate.validation';
+import DateRangePickerField from '@/components/form/fields/DateRangePickerField';
 
 const ESTIMATE_FILTER_FORM_ID = 'estimate-filter';
 
 type Props = {
   onSubmit: (values: any) => void;
 };
+
 const EstimateAdvancedFilterForm = ({ onSubmit }: Props) => {
   const { t } = useTranslation();
   const { open: isOpenFilterDialog, toggle: toggleOpenFilterDialog } = useToggle();
@@ -47,7 +48,8 @@ const EstimateAdvancedFilterForm = ({ onSubmit }: Props) => {
         formId={ESTIMATE_FILTER_FORM_ID}>
         <Form formId={ESTIMATE_FILTER_FORM_ID} form={form} onSubmit={handleSubmit(onSubmitHandler)}>
           <Stack direction="row" spacing={2}>
-            <TextField name="url" label={t('common:link')} type="text" variant="outlined" fullWidth fixedLabel />
+            <DateRangePickerField name="createdAt" label={t('common:createdAt')} variant="outlined" fullWidth />
+            <DateRangePickerField name="updatedAt" label={t('common:updatedAt')} variant="outlined" fullWidth />
           </Stack>
         </Form>
       </Dialog>
