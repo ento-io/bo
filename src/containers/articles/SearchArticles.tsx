c092b1a6da2315d6ec86285b66880a5836551a37
tiavina-mika <tiavinamika@gmail.com>
2024-04-06 12:02:42 +0300

    Create SearchArticles.tsx

---
+++ src/containers/articles/SearchArticles.tsx
@@ -0,0 +1,25 @@
+import { Stack } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import SearchInput from '@/components/form/inputs/SearchInput';
+
+import ArticleAdvancedFilterForm from '@/containers/articles/ArticleAdvancedFilterForm';
+import { IRenderSearchProps } from '@/types/app.type';
+
+const SearchArticles = ({ onSearch, onAdvancedSearch }: IRenderSearchProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Stack direction="row" spacing={2}>
+        <SearchInput
+          onChange={onSearch}
+          name="user"
+          placeholder={t('user:searchByNameOrEmail')}
+        />
+      </Stack>
+      <ArticleAdvancedFilterForm onSubmit={onAdvancedSearch} />
+    </>
+  );
+}
+
+export default SearchArticles;
