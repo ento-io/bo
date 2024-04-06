cc0bd8f24c42ce7dbd4a7a27daa36c6014a51b95
tiavina-mika <tiavinamika@gmail.com>
2024-04-06 12:03:14 +0300

    Update article.validations.ts

--- src/validations/article.validations.ts
+++ src/validations/article.validations.ts
@@ -1,5 +1,13 @@
 import { z } from "zod";
+import { dateForAdvancedSearchSchema } from "./app.validations";
 
 export const articleSchema = z.object({
   title: z.string().min(1, { message: 'Title required' }),
+});
+
+export const articleFilterSchema = z.object({
+  createdAt: dateForAdvancedSearchSchema,
+  updatedAt: dateForAdvancedSearchSchema,
+  user: z.string().optional(),
+  status: z.array(z.string().optional()).optional(),
 });
