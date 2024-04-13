504526d51ded98785a7115f8ee9a33b2d497e789
tiavina-mika <tiavinamika@gmail.com>
2024-04-13 20:08:50 +0300

    Update theme.utils.ts

--- src/utils/theme.utils.ts
+++ src/utils/theme.utils.ts
@@ -129,6 +129,51 @@ const defaultTheme = {
     ].join(','),
   },
   components: {
+    MuiSwitch: {
+      styleOverrides: {
+        root: ({ theme, }: { theme: Theme, ownerState: BadgeProps }) => ({
+          width: 38,
+          height: 20,
+          padding: 0,
+          display: 'flex',
+          '&:active': {
+            '& .MuiSwitch-thumb': {
+              width: 19,
+            },
+            '& .MuiSwitch-switchBase.Mui-checked': {
+              transform: 'translateX(9px)',
+            },
+          },
+          '& .MuiSwitch-switchBase': {
+            padding: 1,
+            '&.Mui-checked': {
+              transform: 'translateX(16px)',
+              color: '#fff',
+              '& + .MuiSwitch-track': {
+                opacity: 1,
+                backgroundColor: theme.palette.mode === 'dark' ? '#177ddc' : '#1890ff',
+              },
+            },
+          },
+          '& .MuiSwitch-thumb': {
+            boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
+            width: 18,
+            height: 18,
+            borderRadius: 20 / 2,
+            transition: theme.transitions.create(['width'], {
+              duration: 200,
+            }),
+          },
+          '& .MuiSwitch-track': {
+            borderRadius: 20 / 2,
+            opacity: 1,
+            backgroundColor:
+              theme.palette.mode === 'dark' ? 'rgba(255,255,255,.35)' : 'rgba(0,0,0,.25)',
+            boxSizing: 'border-box',
+          },
+        }),
+      }
+    },
     MuiChip: {
       styleOverrides: {
         root: {
