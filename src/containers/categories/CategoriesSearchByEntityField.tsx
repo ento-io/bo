fe3376061eaac1f559daae2d763889543ad4d3b9
tiavina-mika <tiavinamika@gmail.com>
2024-04-15 12:43:56 +0300

    Create CategoriesSearchByEntityField.tsx

---
+++ src/containers/categories/CategoriesSearchByEntityField.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react"
+
+import { debounce } from "lodash";
+import { useSelector } from "react-redux";
+import AutocompleteField, { AutocompleteProps } from "@/components/form/fields/AutocompleteField";
+import { searchCategoriesForAutocomplete } from "@/redux/actions/category.action";
+import { getTranslatedField } from "@/utils/settings.utils";
+import { ISelectOption } from "@/types/app.type";
+import { CategoryEntityEnum, ICategory } from "@/types/category.types";
+import { getSettingsLangSelector } from "@/redux/reducers/settings.reducer";
+
+type ICategoryOptionValue = {
+  objectId: string;
+}
+type ICategoryOption = ISelectOption<ICategoryOptionValue>;
+
+type Props = {
+  entity: CategoryEntityEnum.Article;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  multiple?: boolean;
+} & Omit<AutocompleteProps<ICategoryOptionValue>, 'options'>
+
+const CategoriesSearchByEntityField = ({ entity, name, label, placeholder, multiple = true, ...otherProps }: Props) => {
+  const language = useSelector(getSettingsLangSelector);
+
+  const [categoryOptions, setCategoryOptions] = useState<ICategoryOption[]>([]);
+  const [categoryOptionsLoading, setCategoryOptionsLoading] = useState<boolean>(false);
+
+  /** autocomplete search */
+  const handleSearchCategory = debounce(async (search: string) => {
+    setCategoryOptionsLoading(true);
+    const categories = await searchCategoriesForAutocomplete(search, entity) || [];
+    const newOptions = categories.map((category: ICategory) => ({
+      value: {
+        objectId: category.objectId,
+      },
+      label: getTranslatedField(category.translated, language, 'name')
+    }));
+    setCategoryOptions(newOptions);
+    setCategoryOptionsLoading(false);
+  }, 500)
+
+  return (
+    <AutocompleteField<ICategoryOptionValue>
+      {...otherProps}
+      name={name}
+      label={label}
+      placeholder={placeholder}
+      disableNoOptions
+      loading={categoryOptionsLoading}
+      options={categoryOptions}
+      fullWidth
+      onSearch={handleSearchCategory}
+      multiple={multiple}
+    />
+  )
+}
+
+export default CategoriesSearchByEntityField;
