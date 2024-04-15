f749036a4406ad8d2abeecf92b5c1a5c183cd122
tiavina-mika <tiavinamika@gmail.com>
2024-04-15 10:01:41 +0300

    Update category.types.ts

--- src/types/category.types.ts
+++ src/types/category.types.ts
@@ -2,11 +2,19 @@ import { Attributes } from "parse";
 import { z } from "zod";
 import { IUser } from "./user.type";
 import { categoryFilterSchema, categorySchema } from "@/validations/category.validation";
+import { ISelectOption } from "./app.type";
 
 export interface ITranslatedFields {
   name: string;
 }
 
+export enum CategoryEntityEnum {
+  Article = "article",
+  Page = "page",
+}
+
+type ICategoryTypeEntity = `${CategoryEntityEnum}`;
+export type ICategoryEntityOption = ISelectOption<ICategoryTypeEntity>;
 export interface ICategory extends Attributes {
   objectId: string;
   updatedAt?: string;
@@ -17,6 +25,8 @@ export interface ICategory extends Attributes {
   deletedAt?: string;
   deleted: boolean;
   translated: ITranslatedFields;
+  // each category can be linked to an article, a page, product, ...
+  entity: CategoryEntityEnum;
 }
 
 export interface ICategoryState {
