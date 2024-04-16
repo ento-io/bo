1ca38e415d83e5bf7e7dab3cb74d0ec9db4804b3
tiavina-mika <tiavinamika@gmail.com>
2024-04-16 17:45:07 +0300

    Update pathnames.ts

--- src/utils/pathnames.ts
+++ src/utils/pathnames.ts
@@ -4,6 +4,7 @@ export const PATH_NAMES = {
   home: '/',
   users: '/' + i18n.t('route:users'),
   articles: '/' + i18n.t('route:articles'),
+  pages: '/' + i18n.t('route:pages'),
   categories: '/' + i18n.t('route:categories'),
   admins: '/' + i18n.t('route:admins'),
   login: '/' + i18n.t('route:login'),
