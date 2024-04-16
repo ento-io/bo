30550f5165138d1594c2f6e22916efacad496ff9
tiavina-mika <tiavinamika@gmail.com>
2024-04-16 17:44:59 +0300

    Create page.types.ts

---
+++ src/types/page.types.ts
@@ -0,0 +1,40 @@
+import { Attributes } from "parse";
+import { z } from "zod";
+import { IUser } from "./user.type";
+import { articleFilterSchema, articleSchema } from "@/validations/article.validations";
+import { IFileCloud } from "./file.type";
+import { ICategory } from "./category.types";
+import { ISEOFields } from "./app.type";
+
+export interface IPageTranslatedFields extends ISEOFields {
+  title: string;
+  content: string;
+}
+
+export interface IPage extends Attributes {
+  objectId: string;
+  title: string;
+  updatedAt?: string;
+  createdAt?: string;
+  user: IUser;
+  updatedBy?: IUser;
+  deletedBy?: IUser;
+  deletedAt?: string;
+  deleted: boolean;
+  translated: IPageTranslatedFields;
+  bannerImage?: IFileCloud;
+  previewImage?: IFileCloud;
+  images?: IFileCloud[];
+  categories: ICategory[];
+}
+
+export interface IPageState {
+  loading: boolean;
+  page: IPage | null;
+  pages: IPage[];
+  count: number;
+}
+
+export type IPageInput = z.infer<typeof articleSchema>;
+export type PageFiltersInput = z.infer<typeof articleFilterSchema>;
+
