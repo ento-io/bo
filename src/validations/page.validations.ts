e8c0c059640dc4aded88f5fc75739eedcac09f2c
tiavina-mika <tiavinamika@gmail.com>
2024-04-17 18:24:33 +0300

    Update page.validations.ts

--- src/validations/page.validations.ts
+++ src/validations/page.validations.ts
@@ -1,16 +1,30 @@
 import { z } from "zod";
 import { articleFilterSchema, cmsSchema, getCMSTranslatedSchema } from "./article.validations";
-import { formatTranslatedFormValuesToSave } from "@/utils/cms.utils";
+import { formatTranslatedPageFormValuesToSave } from "@/utils/cms.utils";
 import { categoryOptionSchema } from "./category.validation";
 import { CategoryEntityEnum } from "@/types/category.type";
+import { locales } from "@/config/i18n";
 
 export const pageFilterSchema = articleFilterSchema.extend({
   metaTitle: z.string().optional(),
 });
 
+const getTranslatedBlockSchema = () => {
+  const values: Record<string, any> = {};
+  for (const locale of locales) {
+    values[locale + ':name'] = z.string().optional();
+  }
+  return values
+}
+
 export const pageSchema = cmsSchema
   .extend({
     ...getCMSTranslatedSchema(CategoryEntityEnum.Page), // translated fields
-    category: categoryOptionSchema.optional(),
+    category: z.preprocess(
+      val => {
+        return val === '' ? undefined : val
+      },
+      categoryOptionSchema.optional()),
+    blocks: z.array(z.object(getTranslatedBlockSchema())).optional()
   })
-  .transform(formatTranslatedFormValuesToSave);
+  .transform(formatTranslatedPageFormValuesToSave);
