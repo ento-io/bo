4a0409ab0fd24ea4e1239474aa4d6667593e5222
tiavina-mika <tiavinamika@gmail.com>
2024-04-17 19:12:15 +0300

    Update page.validations.ts

--- src/validations/page.validations.ts
+++ src/validations/page.validations.ts
@@ -1,30 +1,51 @@
 import { z } from "zod";
+import { capitalize } from "string-ts";
 import { articleFilterSchema, cmsSchema, getCMSTranslatedSchema } from "./article.validations";
 import { formatTranslatedPageFormValuesToSave } from "@/utils/cms.utils";
 import { categoryOptionSchema } from "./category.validation";
 import { CategoryEntityEnum } from "@/types/category.type";
-import { locales } from "@/config/i18n";
+import i18n, { locales } from "@/config/i18n";
+import { DEFAULT_LANGUAGE } from "@/utils/constants";
+import { errorMap } from '@/config/zod';
 
 export const pageFilterSchema = articleFilterSchema.extend({
   metaTitle: z.string().optional(),
 });
 
+/**
+ * each translated fields of a block
+ * ex: { 'fr:name': 'xxx', 'en:name': 'yyy' }
+ * @returns 
+ */
 const getTranslatedBlockSchema = () => {
-  const values: Record<string, any> = {};
+  const translatedSchema: Record<string, any> = {};
+
   for (const locale of locales) {
-    values[locale + ':name'] = z.string().optional();
+    if (locale !== DEFAULT_LANGUAGE) {
+      translatedSchema[locale + ':name'] = z.string({ errorMap }).transform(capitalize);
+    } else {
+      // required fields only for default locale
+      [{
+        key: 'name',
+        label: i18n.t('cms:pageName'),
+      }].forEach(({ key, label }) => {
+          translatedSchema[locale + ':' + key] = z.string({ errorMap })
+          .min(1, i18n.t('form.error.required', { field: label }))
+      });
+    }
   }
-  return values
+  return translatedSchema
 }
 
-export const pageSchema = cmsSchema
+export const pageSchema: any = cmsSchema
   .extend({
     ...getCMSTranslatedSchema(CategoryEntityEnum.Page), // translated fields
+    // optional category
     category: z.preprocess(
-      val => {
-        return val === '' ? undefined : val
-      },
+      // ISSUE: https://stackoverflow.com/questions/74256866/validating-optional-fields-in-react-using-zod
+      (value) => value === '' ? undefined : value,
       categoryOptionSchema.optional()),
+    // ex: [{ 'fr:name': 'xxx', 'en:name': 'yyy' }, { 'fr:name': 'xxx', 'en:name': 'yyy' }]
     blocks: z.array(z.object(getTranslatedBlockSchema())).optional()
   })
   .transform(formatTranslatedPageFormValuesToSave);
